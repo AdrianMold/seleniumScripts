7b66c9205a522a510f40461d786b6aa8286b0aad
Moldovan iulia <you@example.com>
2021-06-03 20:10:07 +0200

    add some files : Condition1, etc

---
+++ Condition2.py
@@ -0,0 +1,12 @@
+name = "John"
+age = 24
+if name == "John" and age == 24:
+    #print("Your name is John, and you are also 24 years old.")
+    pass
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
+
+name = "John"
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick.")
---
+++ Loops1.py
@@ -0,0 +1,3 @@
+# Prints out the numbers 0,1,2,3,4
+for x in range(8):
+    print(x)
